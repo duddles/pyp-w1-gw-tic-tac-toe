0583c4d6c2e78b91c2957fa9083d80aa88be5c4d
Martin Zugnoni <martin.zugnoni@gmail.com>
2016-05-18 15:56:55 -0300

    Add docstrings in main.py

--- main.py
+++ main.py
@@ -10,41 +10,105 @@ class GameOver(Exception):
 
 # internal helpers
 def _position_is_empty_in_board(position, board):
+    """
+    Checks if given position is empty ("-") in the board.
+
+    :param position: Two-elements tuple representing a
+                     position in the board. Example: (0, 1)
+    :param board: Game board.
+
+    Returns True if given position is empty, False otherwise.
+    """
     pass
 
 
 def _position_is_valid(position):
+    """
+    Checks if given position is a valid. To consider a position as valid, it
+    must be a two-elements tuple, containing values from 0 to 2.
+    Examples of valid positions: (0,0), (1,0)
+    Examples of invalid positions: (0,0,1), (9,8), False
+
+    :param position: Two-elements tuple representing a
+                     position in the board. Example: (0, 1)
+
+    Returns True if given position is valid, False otherwise.
+    """
     pass
 
 
 def _board_is_full(board):
+    """
+    Returns True if all positions in given board are occupied.
+
+    :param board: Game board.
+    """
     pass
 
 
 def _is_winning_combination(board, combination, player):
+    """
+    Checks if all 3 positions in given combination are occupied by given player.
+
+    :param board: Game board.
+    :param combination: Tuple containing three position elements.
+                        Example: ((0,0), (0,1), (0,2))
+
+    Returns True of all three positions in the combination belongs to given
+    player, False otherwise.
+    """
     pass
 
 
 def _check_winning_combinations(board, player):
+    """
+    There are 8 posible combinations (3 horizontals, 3, verticals and 2 diagonals)
+    to win the Tic-tac-toe game.
+    This helper loops through all these combinations and checks if any of them
+    belongs to the given player.
+
+    :param board: Game board.
+    :param player: One of the two playing players.
+
+    Returns the player (winner) of any of the winning combinations is completed
+    by given player, or None otherwise.
+    """
     pass
 
 
 # public interface
 def start_new_game(player1, player2):
+    """
+    Creates and returns a new game configuration.
+    """
     pass
 
 
 def get_winner(game):
+    """
+    Returns the winner player if any, or None otherwise.
+    """
     pass
 
 
 def move(game, player, position):
+    """
+    Performs a player movement in the game. Must ensure all the pre requisites
+    checks before the actual movement is done.
+    After registering the movement it must check if the game is over.
+    """
     pass
 
 
 def get_board_as_string(game):
+    """
+    Returns a string representation of the game board in the current state.
+    """
     pass
 
 
 def get_next_turn(game):
+    """
+    Returns the player who plays next, or None if the game is already over.
+    """
     pass
